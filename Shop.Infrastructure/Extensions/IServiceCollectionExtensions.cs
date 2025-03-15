1090714f4d23ebd9d62c7ba2eb3ade6444aa2441
AbbasKashi69 <AbbasKashi69@gmail.com>
2025-03-15 13:05:15 +0330

    added infrastructre layer

---
+++ Shop.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
+using Shop.Infrastructure.Services;
+
+namespace Shop.Infrastructure.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services)
+        {
+            services.AddServices();
+        }
+
+        private static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IEmailService, EmailService>();
+        }
+    }
+}
---
+++ Shop.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Shop.Application.DTOs;
+using Shop.Application.Interfaces;
+
+namespace Shop.Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendAsync(EmailRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
